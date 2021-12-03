146ac6b41783621f830ad4d263531a90887b6b46
Laurin Wolf <laurin@l5w.de>
2021-12-03 12:03:14 +0100

    improve pressure display, #2

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -8,7 +8,47 @@
 #include "hal/lcd.hal.hpp"
 #include "hal/piezo.hal.hpp"
 
+#include <math.h>
 #include "stdio.h"
+
+constexpr uint16_t SENSOR_MAX = 4096;
+
+uint16_t round(uint16_t d)
+{
+  return floor(d + 0.5);
+}
+
+uint16_t mapValToRange(uint16_t val, uint16_t valMin, uint16_t valMax, uint16_t rangeMin, uint16_t rangeMax)
+{
+  return (val - valMin) * (rangeMax - rangeMin) / (valMax - valMin) + rangeMin;
+}
+
+void getPressureDisplayStr(char *buf, uint16_t val, uint16_t maxVal, uint8_t width, uint16_t constrLower, uint16_t constrUpper)
+{
+  uint16_t mappedVal = mapValToRange(val, 0, SENSOR_MAX, 0, width);
+  uint16_t mappedConstrLower = mapValToRange(constrLower, 0, SENSOR_MAX, 0, width);
+  uint16_t mappedConstrUpper = mapValToRange(constrUpper, 0, SENSOR_MAX, 0, width);
+
+  for (uint8_t i = 0; i < width; i++)
+  {
+    if (i == mappedVal)
+    {
+      buf[i] = '|';
+    }
+    else if (i == mappedConstrLower)
+    {
+      buf[i] = '>';
+    }
+    else if (i == mappedConstrUpper)
+    {
+      buf[i] = '<';
+    }
+    else
+    {
+      buf[i] = ' ';
+    }
+  }
+}
 
 void firmwareSetup()
 {
@@ -31,17 +71,17 @@ void firmwareSetup()
     onboardLED->toggle();
     sensorValue = HAL::AdConverter::getValue();
 
-    HAL::USART::clearScreen();
-    HAL::USART::printf("Druck: %d\r\n", sensorValue);
+    char bar[LCD_CHARACTERS_PER_LINE];
+    getPressureDisplayStr(bar, sensorValue, SENSOR_MAX, LCD_CHARACTERS_PER_LINE, .2 * SENSOR_MAX, .8 * SENSOR_MAX);
+
     HAL::LcDisplay::clearDisplay();
     HAL::LcDisplay::printf("Druck: %d", sensorValue);
-    char bar[LCD_CHARACTERS_PER_LINE];
-    for (size_t i = 0; i < LCD_CHARACTERS_PER_LINE; i++)
-    {
-      bar[i] = sensorValue * LCD_CHARACTERS_PER_LINE / 4096u > i ? 0xFF : 0x00;
-    }
     HAL::LcDisplay::setCursor(1, 0);
     HAL::LcDisplay::print(bar);
+
+    HAL::USART::clearScreen();
+    HAL::USART::printf("Druck: %d\r\n", sensorValue);
+    HAL::USART::print(bar, LCD_CHARACTERS_PER_LINE);
 
     HAL::Piezo::setEnabled(sensorValue > 3500);
 
